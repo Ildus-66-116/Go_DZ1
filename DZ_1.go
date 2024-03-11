7a3fd90826a17041414f1987d2277e216efc204f
ILDUS <ildus6696@gmail.com>
2024-03-11 11:02:25 +0300

    1 reshenie

--- DZ_1.go
+++ DZ_1.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+		if len(os.Args) < 2 {
 		log.Fatal("Укажите полный путь до файла вторым аргументом")
 	}
-
 	filePth := os.Args[1]
-
 	var fileName, fileExt string
+	indE := strings.LastIndex(filePth, ".")
+	fileExt = filePth[indE+1:]
+	indN := strings.LastIndex(filePth, "/")
+	fileName = filePth[indN+1:indE]
 	// Напишите код, который выведет следующее
 	// filename: <name>
 	// extension: <extension>
@@ -21,7 +24,6 @@ func main() {
 	// Подсказка. Возможно вам понадобится функция strings.LastIndex
 	// Для проверки своего решения используйте функции filepath.Base() filepath.Ext(
 	// ) Они могут помочь для проверки решения
-
 	fmt.Printf("filename: %s\n", fileName)
 	fmt.Printf("extension: %s\n", fileExt)
 }
